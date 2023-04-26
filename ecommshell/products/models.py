b380f9747dd6a906ebeb879329151633413087bb
ialoysious <isaacaloysious@gmail.com>
2023-04-26 16:29:08 -0700

    changing model structure, probably a bad idea

--- ecommshell/products/models.py
+++ ecommshell/products/models.py
@@ -1,4 +1,67 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
+
+class Category(MPTTModel):
+    """
+    Category Model
+    """
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    is_active = models.BooleanField(default=True)
+
+    class MPPTMeta:
+        order_insertion_by = ['name']
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        """
+        Return absolute url
+        """
+        return reverse('products:category_list', args=[self.slug])
+    def __str__(self):
+        return self.name
+    
+class ProductType(models.Model):
+    """ 
+    Product Type Model
+    
+    """
+    name = models.CharField(verbose_name=_("Product Name"),help_text=_("Required"), max_length=255, unique=True)
+    is_active = models.BooleanField(verbose_name=_("Active"), default=True)
+
+    class Meta:
+        verbose_name = _("Product Type")
+        verbose_name_plural = _("Product Types")
+
+    def __str__(self):
+        return self.name
+    
+class ProductSpecification(models.Model):
+    """ 
+    Product Specification Model
+    
+    """
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    name = models.CharField(verbose_name=_("Name"), help_text=_("Required"), max_length=255)
+
+    class Meta:
+        verbose_name = _("Product Specification")
+        verbose_name_plural = _("Product Specifications")
+        ordering = ("product_type", "name")
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
 
 
 class Product(models.Model):
