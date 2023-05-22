2f821da1f3a36723cba4aa113eca6b41c31b33f1
ialoysious <isaacaloysious@gmail.com>
2023-05-22 14:18:13 -0700

    starting cart and stripe integration

--- ecommshell/cart/urls.py
+++ ecommshell/cart/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     # ...
     path('add/<int:product_id>/', views.AddToCartView.as_view(), name='add_to_cart'),
     path('', views.CartView.as_view(), name='cart'),
+    path('checkout/', views.CreateCheckoutSessionView.as_view(), name='create_checkout_session'),
+    path('webhook/stripe/', views.WebHook.as_view()),
     
     # ...
 ]
